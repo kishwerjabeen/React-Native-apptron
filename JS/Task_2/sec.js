9e70012bd24bb1efe727469104067ca45ceba820
Kishwar Jabeen <43556329+kishwerjabeen@users.noreply.github.com>
2019-04-06 22:29:10 -0700

    Add files via upload

---
+++ JS/Task_2/sec.js
@@ -0,0 +1,65 @@
+// 1 - Declare an empty array using JS literal notation to store student names in future.
+
+var abc =[];
+
+// 2- Declare an empty array using JS object notation to store student names in future.
+
+var abc = new abc();
+
+// 3- Declare and initialize a strings array.
+
+var abc	= ["kishwar", "jabeen"];
+alert(abc[0]);
+alert("welcome");
+
+// 4- Declare and initialize a numbers array.
+
+var	abc = [1,2,4];
+
+// 5- Declare and initialize a boolean array. 
+
+var boolArray = [true, false];
+
+//6- Declare and initialize a mixed array.
+
+var arrayMix = [1, "one", "ture", "a", 2];
+
+// 7 - Declare and Initialize an array and store available mobile
+// networks in Pakistan.
+
+var networks = ["zong", "ufone", "jazz", "telenor"];
+
+// 8 - Declare and Initialize an array and store available educationqualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser like:
+
+var edu = ["ssc", "HSC", "BCS", "BS", "BCOM", "MS", "mphil", "PhD"];
+
+alert("Qualificatios");
+alert(edu[0]);
+alert(edu[1]);
+alert(edu[2]);
+alert(edu[3]);
+alert(edu[4]);
+alert(edu[5]);
+alert(edu[6]);
+alert(edu[7]);
+
+// Declare and initialize an empty array to store top movies of 2015. Add movies one by one in an array. Display the elements
+// & length of array in your browser. (Use array’s length method)
+
+var arr = [];
+
+arr=["Avengers: Age of 	Ultron"];
+arr=["Spectre"];
+arr=["Jurassic World"];
+arr=["Inside out"];
+
+console.write("<br>" + "<h1>" + "top movies of 2015" + "</h1>" + "</br>");
+console.write("<br>" +  "1)   " + arr[0] + "</br>");
+console.write("<br>" +  "2)   " + arr[2] + "</br>");
+console.write("<br>" +  "3)   " + arr[3] + "</br>");
+
+alert(arr.length);
+
+
+
---
+++ JS/Task_2/task2.js
@@ -0,0 +1,45 @@
+// 1 - Declare an empty array using JS literal notation to store student names in future.
+
+var abc =[];
+
+// 2- Declare an empty array using JS object notation to store student names in future.
+
+var abc = new abc();
+
+// 3- Declare and initialize a strings array.
+
+var abc	= ["kishwar", "jabeen"];
+alert(abc[0]);
+alert("welcome");
+
+// 4- Declare and initialize a numbers array.
+
+var	abc = [1,2,4];
+
+// 5- Declare and initialize a boolean array. 
+
+var boolArray = [true, false];
+
+//6- Declare and initialize a mixed array.
+
+var arrayMix = [1, "one", "ture", "a", 2];
+
+// 7 - Declare and Initialize an array and store available mobile
+// networks in Pakistan.
+
+var networks = [zong, ufone, jazz, telenor];
+
+// 8 - Declare and Initialize an array and store available educationqualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS,
+// M. Phil., PhD). Show the listed qualifications in your browser like:
+
+var edu = [ssc, HSC, BCS, BS, BCOM, MS, mphil, PhD];
+
+alert("Qualificatios");
+alert(edu[0]);
+alert(edu[1]);
+alert(edu[2]);
+alert(edu[3]);
+alert(edu[4]);
+alert(edu[5]);
+alert(edu[6]);
+alert(edu[7]);
